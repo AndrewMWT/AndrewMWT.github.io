46c11a57926d7dd91efa5d7768543c6ac7cd9942
AndrewMWT <armaldonado@my.waketech.edu>
2025-04-23 13:43:27 -0400

    Lesson 12 assignment

---
+++ gradebook.js
@@ -0,0 +1,19 @@
+// Need to: Grab SQL database stuff
+function fetchGradeData() {
+    // This will grab and return database info
+    console.log("Fetching grade data...")
+
+}
+
+// To Do: Populate the table with grade data
+function populateGradebook(data) {
+    //This will take grade data and update the table
+    console.log("Populating gradebook with data:" , data);
+
+}
+
+// To Do REMOVE THIS
+// Call the stubs to demonstrate workflow
+const gradeData = fetchGradeData();
+populateGradebook(gradeData)
+// END REMOVE
